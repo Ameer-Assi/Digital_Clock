bc6cbd9e7b24f6342454685215a68cea89ccf6ca
Ameer Assi <ameerassi@Ameers-MacBook-Air.local>
2026-03-19 16:46:14 +0200

    Fix minCounter bit width's

--- minCounter.sv
+++ minCounter.sv
@@ -1,9 +1,9 @@
 module minCounter (
     input logic clk,
     input logic rst,
-    output logic [3:0] mins
+    output logic [5:0] mins
 );
-    logic [3:0] sec_Counter;
+    logic [5:0] sec_Counter;
     always_ff @(posedge clk) begin 
         if (rst == 1)begin
             mins <= 0;
@@ -14,7 +14,8 @@ module minCounter (
                 sec_Counter <= 0;
                 mins <= mins + 1;
             end
-            sec_Counter <= sec_Counter + 1;
+            else
+                sec_Counter <= sec_Counter + 1;
         end
     end
 
