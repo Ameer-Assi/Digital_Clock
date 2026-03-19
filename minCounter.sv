ee75b9e683a9f8f6f43422bf624b5e98e8d6ee69
Ameer Assi <ameerassi@Ameers-MacBook-Air.local>
2026-03-19 20:30:20 +0200

    Add hours + minutes logic to minCounter

--- minCounter.sv
+++ minCounter.sv
@@ -1,22 +1,32 @@
 module minCounter (
     input logic clk,
     input logic rst,
-    output logic [5:0] mins
-    output logic [5:0] secs
+    input logic tick,
+    output logic [5:0] mins,
+    output logic [5:0] secs,
+    output logic [4:0] hours
 );
     always_ff @(posedge clk) begin 
         if (rst == 1)begin
             mins <= 0;
             secs <= 0;
         end
-        else begin
+        else if(tick == 1) begin
             if (secs == 59) begin
                 secs <= 0;
-                mins <= mins + 1;
+                if(mins == 59) begin
+                    mins <= 0;
+                    if(hours == 23)
+                        hours <= 0;
+                    else
+                        hours <= hours + 1;
+                end
+                else
+                    mins <= mins + 1;
+
             end
             else
                 secs <= secs + 1;
         end
     end
-
 endmodule
