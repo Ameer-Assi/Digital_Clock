12c037b62d51ae84ea885f33ca7d6d75e148028a
Ameer Assi <ameerassi@Ameers-MacBook-Air.local>
2026-03-19 21:16:28 +0200

    Add 7 segment display module support

---
+++ clockDisplay.sv
@@ -0,0 +1,20 @@
+module clockDisplay (
+    input logic [3:0] num,
+    output logic [6:0] pins // 7 segment display, active-high 
+);
+always_comb begin
+    case (num)
+        0: pins = 7'b0111111;
+        1: pins = 7'b0000011;
+        2: pins = 7'b1011011;
+        3: pins = 7'b1001111;
+        4: pins = 7'b1100110;
+        5: pins = 7'b1101101;
+        6: pins = 7'b1111101;
+        7: pins = 7'b1000111;
+        8: pins = 7'b1111111;
+        9: pins = 7'b1100111;
+        default: pins = 7'b1111111;
+    endcase
+end
+endmodule
