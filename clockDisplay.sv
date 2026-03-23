88e6acea11cc4ea521315608251fa52ba1bf7fe6
Ameer Assi <ameerassi@Ameers-MacBook-Air.local>
2026-03-23 14:47:23 +0200

    Add blinking feature

--- clockDisplay.sv
+++ clockDisplay.sv
@@ -1,8 +1,11 @@
 module clockDisplay (
     input logic [3:0] num,
+    input logic blink,
+    input logic tick,
     output logic [6:0] pins // 7 segment display, active-high 
 );
 always_comb begin
+    if(!blink) begin
     case (num)
         0: pins = 7'b0111111;
         1: pins = 7'b0000110;
@@ -16,5 +19,26 @@ always_comb begin
         9: pins = 7'b1100111;
         default: pins = 7'b1111111;
     endcase
+    end
+    else begin
+        if(tick) begin
+            case (num)
+                0: pins = 7'b0111111;
+                1: pins = 7'b0000110;
+                2: pins = 7'b1011011;
+                3: pins = 7'b1001111;
+                4: pins = 7'b1100110;
+                5: pins = 7'b1101101;
+                6: pins = 7'b1111101;
+                7: pins = 7'b0000111;
+                8: pins = 7'b1111111;
+                9: pins = 7'b1100111;
+                default: pins = 7'b1111111;
+            endcase
+        end
+        else begin
+            pins = 0;
+        end
+    end
 end
 endmodule
